dfae9348dc5da085c9e30c5dc60653f9c221c0d7
Kishor Naik <kishor.naik011.net@gmail.com>
2021-06-08 20:12:28 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sol_Demo.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly SecretModel secretModel = null;
+
+        public DemoController(IOptions<SecretModel> options)
+        {
+            secretModel = options.Value;
+        }
+
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return base.Ok(secretModel.Key);
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Controllers/DemoEnvironmentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/demoenvironment")]
+    [ApiController]
+    public class DemoEnvironmentController : ControllerBase
+    {
+        private readonly IWebHostEnvironment webHostEnvironment = null;
+
+        public DemoEnvironmentController(IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return base.Ok(webHostEnvironment.EnvironmentName);
+        }
+    }
+}
